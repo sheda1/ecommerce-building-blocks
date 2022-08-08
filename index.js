230672aad23f4606352d34d92d178d1f6c3082a1
sheda1 <105358106+sheda1@users.noreply.github.com>
2022-08-08 16:37:51 +0530

    task11

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ parentContainer.addEventListener('click',(e)=>{
         const img_src = document.querySelector(`#${id} img`).src;
         const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
         let total_cart_price = document.querySelector('#total-value').innerText;
-        // if (document.querySelector(`#in-cart-${id}`)){
-        //     alert('This item is already added to the cart');
-        //     return
-        // }
+        if (document.querySelector(`#in-cart-${id}`)){
+            alert('This item is already added to the cart');
+            return
+        }
         document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
         const cart_item = document.createElement('div');
         cart_item.classList.add('cart-row');
@@ -45,9 +45,48 @@ parentContainer.addEventListener('click',(e)=>{
     if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
         document.querySelector('#cart').style = "display:block;"
     }
-   
-   
+    if (e.target.className=='cancel'){
+        document.querySelector('#cart').style = "display:none;"
+    }
+    if (e.target.className=='purchase-btn'){
+        if (parseInt(document.querySelector('.cart-number').innerText) === 0){
+            alert('You have Nothing in Cart , Add some products to purchase !');
+            return
+        }
+        alert('Thanks for the purchase')
+        cart_items.innerHTML = ""
+        document.querySelector('.cart-number').innerText = 0
+        document.querySelector('#total-value').innerText = `0`;
+    }
 
-   
+    if (e.target.innerText=='REMOVE'){
+        let total_cart_price = document.querySelector('#total-value').innerText;
+        total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
+        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)-1
+        document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
+        e.target.parentNode.parentNode.remove()
+    }
+})
+window.addEventListener('DOMContentLoaded',() => {
+    axios.get('http://localhost:3000/').then((data) =>{
+        console.log(data);
+       if(data.request.status === 200){
+        const products = data.data;
+        console.log(products);
+        const parentSection = document.getElementById('Products');
+        products.forEach(product => {
+            const productHtml = `
+            <div>
+                <h1>${product.title}</h1>
+                <img src = ${product.imageUrl}></img>
+                <button> Add to cart </button>
+
+
+            </div>`
+            parentSection.innerHTML += productHtml
+        })
+       }
+        
+    })
 })
 
